ea7991d47211f88b26332468405a0c1d24323e43
Seth Posner <smartseth@hotmail.com>
2024-01-22 17:12:59 -0800

    Move loop descriptions to a tooltip

--- src/data/layers/root/acceleron/LoopDescriptions.vue
+++ src/data/layers/root/acceleron/LoopDescriptions.vue
@@ -1,21 +1,33 @@
 <template>
-    <div v-for="loop in loops" :key="loop.id">
-        <component v-if="unref(loop.built)" :is="render(unref(loop.display).description)" />
-    </div>
+    <Tooltip v-if="unref(loopDescriptions).length > 0" :display="joined" :direction="Direction.Up">
+        <div>Hover for Current Effects</div>
+    </Tooltip>
 </template>
 
-<script setup lang="ts" generic="T">
-import { render } from "util/vue";
-import { unref } from "vue";
+<script setup lang="tsx" generic="T">
+import { joinJSX, renderJSX } from "util/vue";
+import { computed, unref } from "vue";
 import { GenericLoop } from "./loop";
+import { Direction } from "util/common";
+import { jsx } from "features/feature";
+import Tooltip from "features/tooltips/Tooltip.vue";
 
-defineProps<{
+const props = defineProps<{
     loops: GenericLoop<T>[]
 }>();
+
+const loopDescriptions = computed(() => props.loops.filter(loop => unref(loop.built)).map(loop => renderJSX(unref(loop.display).description)));
+const joined = jsx(() => joinJSX(unref(loopDescriptions), <><br/></>));
 </script>
 
 <style scoped>
 div {
+    color: var(--link);
+}
+
+:deep(.tooltip) {
+    background-color: rgb(from var(--tooltip-background) r g b / 0.9);
+    border-radius: 5px;
     font-size: 12px;
     color: var(--link);
 }
