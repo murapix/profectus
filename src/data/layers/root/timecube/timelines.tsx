891fa055a03a0082df84a6fdf9b57e5761216029
Seth Posner <smartseth@hotmail.com>
2024-02-06 12:11:47 -0800

    Implement timeline data

--- src/data/layers/root/timecube/timelines.tsx
+++ src/data/layers/root/timecube/timelines.tsx
@@ -1,4 +1,4 @@
-import { StyleValue, jsx } from "features/feature";
+import { jsx } from "features/feature";
 import { createReset } from "features/reset";
 import { BaseLayer, createLayer } from "game/layers";
 import skyrmion from "../skyrmion/skyrmion";
@@ -6,16 +6,14 @@ import fome, { FomeTypes } from "../fome/fome";
 import acceleron from "../acceleron/acceleron";
 import timecube from "./timecube";
 import inflaton from "../inflaton/inflaton";
-import { GenericClickable, createClickable } from "features/clickables/clickable";
+import { createClickable } from "features/clickables/clickable";
 import { render, renderRow } from "util/vue";
 import { Sides } from "./timesquares";
-import { Persistent, persistent } from "game/persistence";
 import Decimal, { DecimalSource } from "lib/break_eternity";
 import SpacerVue from "components/layout/Spacer.vue";
-import { format } from "util/break_eternity";
-import { ComputedRef, computed, unref } from "vue";
+import { computed, unref } from "vue";
 import { createExponentialModifier, createMultiplicativeModifier, createSequentialModifier } from "game/modifiers";
-import { createTimeline } from "./timeline";
+import { GenericTimeline, createTimeline } from "./timeline";
 
 const id = "timeline";
 const layer = createLayer(id, function (this: BaseLayer) {
@@ -35,16 +33,74 @@ const layer = createLayer(id, function (this: BaseLayer) {
         bottomRight: createTimeline({sides: [Sides.BOTTOM, Sides.RIGHT]})
     };
 
-    const inTimeline = computed(() => Object.values(timelines).some(timeline => unref(timeline.active)));
-    const depths: Record<Sides, ComputedRef<number>> = {
-        [Sides.FRONT]: computed(() => [timelines.topFront, timelines.frontLeft, timelines.frontRight, timelines.bottomFront].filter(timeline => unref(timeline.active)).length),
-        [Sides.RIGHT]: computed(() => [timelines.topRight, timelines.frontRight, timelines.backRight, timelines.bottomRight].filter(timeline => unref(timeline.active)).length),
-        [Sides.TOP]: computed(() => [timelines.topLeft, timelines.topFront, timelines.topBack, timelines.topRight].filter(timeline => unref(timeline.active)).length),
-        [Sides.BACK]: computed(() => [timelines.topBack, timelines.backLeft, timelines.backRight, timelines.bottomBack].filter(timeline => unref(timeline.active)).length),
-        [Sides.LEFT]: computed(() => [timelines.topLeft, timelines.frontLeft, timelines.backLeft, timelines.bottomLeft].filter(timeline => unref(timeline.active)).length),
-        [Sides.BOTTOM]: computed(() => [timelines.bottomLeft, timelines.bottomFront, timelines.bottomBack, timelines.bottomRight].filter(timeline => unref(timeline.active)).length)
-    };
+    const timelineSides: Record<Sides, GenericTimeline[]> = {
+        [Sides.FRONT]: [timelines.topFront, timelines.frontLeft, timelines.frontRight, timelines.bottomFront],
+        [Sides.RIGHT]: [timelines.topRight, timelines.frontRight, timelines.backRight, timelines.bottomRight],
+        [Sides.TOP]: [timelines.topLeft, timelines.topFront, timelines.topBack, timelines.topRight],
+        [Sides.BACK]: [timelines.topBack, timelines.backLeft, timelines.backRight, timelines.bottomBack],
+        [Sides.LEFT]: [timelines.topLeft, timelines.frontLeft, timelines.backLeft, timelines.bottomLeft],
+        [Sides.BOTTOM]: [timelines.bottomLeft, timelines.bottomFront, timelines.bottomBack, timelines.bottomRight]
+    }
 
+    const activeDepths = Object.fromEntries(
+        Object.entries(timelineSides).map(
+            ([side, timelines]) => [side, computed(() => (timelines as GenericTimeline[]).filter(
+                timeline => unref(timeline.active)
+            ).length)]
+        )
+    );
+    const nextDepths = Object.fromEntries(
+        Object.entries(timelineSides).map(
+            ([side, timelines]) => [side, computed(() => (timelines as GenericTimeline[]).filter(
+                timeline => unref(timeline.next)
+            ).length)]
+        )
+    );
+
+    const sidedScoreMulti: Record<Sides, DecimalSource> = {
+        [Sides.FRONT]: 100,
+        [Sides.RIGHT]: 0.01,
+        [Sides.TOP]: 1,
+        [Sides.BACK]: 1,
+        [Sides.LEFT]: 1000,
+        [Sides.BOTTOM]: 1
+    };
+    const scores = Object.fromEntries(
+        Object.entries(timelineSides).map(
+            ([side, timelines]) => [side, computed(() => (timelines as GenericTimeline[]).reduce(
+                (sum, timeline) => sum.plus(unref(timeline.score)), Decimal.dOne)
+            )]
+        )
+    );
+    const passiveBuffs = Object.fromEntries(
+        Object.entries(scores).map(
+            ([side, score]) => [side, computed(() => unref(score).times(sidedScoreMulti[side as Sides]))]
+        )
+    );
+
+    const depthNerfs: Record<Sides, DecimalSource[]> = {
+        [Sides.FRONT]: [1, 1e8, 1e80, '1e800', '1e8000'],
+        [Sides.RIGHT]: [1, 1e12, 1e120, '1e1200', '1e12000'],
+        [Sides.TOP]: [1, 1e3, 1e30, 1e300, '1e3000'],
+        [Sides.BACK]: [1, 1e6, 1e60, '1e600', '1e6000'],
+        [Sides.LEFT]: [1, 1e2, 1e20, 1e200, '1e2000'],
+        [Sides.BOTTOM]: [1, 1e5, 1e50, '1e500', '1e5000']
+    };
+    const activeDepthNerfs = Object.fromEntries(
+        Object.entries(activeDepths).map(
+            ([side, depth]) => [side, computed(() => 
+                unref(depthNerfs[side as Sides])[unref(depth)]
+            )]
+        )
+    );
+    const nextDepthNerfs = Object.fromEntries(
+        Object.entries(nextDepths).map(
+            ([side, depth]) => [side, computed(() => 
+                unref(depthNerfs[side as Sides])[unref(depth)]
+            )]
+        )
+    );
+    
     const currentScoreModifiers = createSequentialModifier(() => [
         createMultiplicativeModifier(() => ({
             multiplier() { return [unref(skyrmion.pion.pions), unref(skyrmion.spinor.spinors)]
@@ -79,10 +135,17 @@ const layer = createLayer(id, function (this: BaseLayer) {
             description: jsx(() => <>Number of active Timelines</>)
         }))
     ]);
-    const currentScore = currentScoreModifiers.apply(1e-4);
+    const currentScore = computed(() => currentScoreModifiers.apply(1e-4));
 
     const enterTimeline = createClickable(() => ({
-        display: 'Enter Timeline',
+        display() {
+            if (Object.values(timelines).some(timeline => unref(timeline.next))) return 'Enter Timeline';
+            if (Object.values(timelines).some(timeline => unref(timeline.active))) return 'Exit Timeline';
+            return 'Enter Timeline';
+        },
+        canClick() {
+            return Object.values(timelines).some(timeline => unref(timeline.next) || unref(timeline.active))
+        },
         onClick() {
             const score = new Decimal(unref(currentScore));
             for (const timeline of Object.values(timelines)) {
@@ -91,6 +154,7 @@ const layer = createLayer(id, function (this: BaseLayer) {
             }
             reset.reset();
             fome.protoversal.upgrades.reform.amount.value = Decimal.dOne;
+            inflaton.inflatons.value = Decimal.dOne;
             for (const timeline of Object.values(timelines)) {
                 timeline.active.value = unref(timeline.next);
             }
@@ -132,6 +196,10 @@ const layer = createLayer(id, function (this: BaseLayer) {
 
     return {
         timelines,
+        scores,
+        buffs: passiveBuffs,
+        nerfs: activeDepthNerfs,
+        scoreMultipliers: sidedScoreMulti,
         display: jsx(() => (
             <>
                 {render(enterTimeline)}
